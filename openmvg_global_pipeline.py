a263da46faa92ca922cf7d551c1d0d1a3c117923
Sparks-Lu <lugm@vhuantech.com>
2022-05-17 23:15:34 +0800

    Added time output.

--- openmvg_global_pipeline.py
+++ openmvg_global_pipeline.py
@@ -18,24 +18,25 @@
 #
 
 # Indicate the openMVG binary directory
-OPENMVG_SFM_BIN = "./openMVG_Build/Linux-x86_64-Release"
+OPENMVG_SFM_BIN = './openMVG_Build/Linux-x86_64-Release'
 
 # Indicate the openMVG camera sensor width directory
-CAMERA_SENSOR_WIDTH_DIRECTORY = "./openMVG/exif/sensor_width_database"
+CAMERA_SENSOR_WIDTH_DIRECTORY = './openMVG/exif/sensor_width_database'
 
 import os
 import subprocess
 import sys
+import time
 
 
 class GlobalReconstructor(object):
     def __init__(self, input_dir, output_dir):
         self.input_dir = input_dir
         self.output_dir = output_dir
-        self.matches_dir = os.path.join(self.output_dir, "matches")
-        self.mvs_dir = os.path.join(self.input_dir, "../openMVS")
-        self.reconstruction_dir = os.path.join(self.output_dir, "reconstruction_global")
-        self.camera_file_params = os.path.join(CAMERA_SENSOR_WIDTH_DIRECTORY, "sensor_width_camera_database.txt")
+        self.matches_dir = os.path.join(self.output_dir, 'matches')
+        self.mvs_dir = os.path.join(self.input_dir, '../openMVS')
+        self.reconstruction_dir = os.path.join(self.output_dir, 'reconstruction_global')
+        self.camera_file_params = os.path.join(CAMERA_SENSOR_WIDTH_DIRECTORY, 'sensor_width_camera_database.txt')
         # Create the ouput/matches folder if not present
         if not os.path.exists(self.output_dir):
           os.mkdir(self.output_dir)
@@ -49,110 +50,134 @@ class GlobalReconstructor(object):
         pass
 
     def list_images(self):
-        print ("1. Intrinsics analysis")
+        time_start = time.time()
+        print ('[{}] 1. Intrinsics analysis'.format(time_start))
         pIntrisics = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_SfMInit_ImageListing"),
-                    "-i", self.input_dir,
-                    "-o", self.matches_dir,
-                    "-d", self.camera_file_params
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_SfMInit_ImageListing'),
+                    '-i', self.input_dir,
+                    '-o', self.matches_dir,
+                    '-d', self.camera_file_params
                     ])
         pIntrisics.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
     def compute_features(self):
-        print ("2. Compute features")
+        time_start = time.time()
+        print ('[{}] 2. Compute features'.format(time_start))
         pFeatures = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_ComputeFeatures"),
-                    "-i", self.matches_dir + "/sfm_data.json",
-                    "-o", self.matches_dir,
-                    "-m", "SIFT"
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_ComputeFeatures'),
+                    '-i', self.matches_dir + '/sfm_data.json',
+                    '-o', self.matches_dir,
+                    '-m', 'SIFT'
                     ])
         pFeatures.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def compute_pairs(self):
-        print ("3. Compute matching pairs")
+        time_start = time.time()
+        print ('[{}] 3. Compute matching pairs'.format(time_start))
         pPairs = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_PairGenerator"),
-                    "-i", self.matches_dir + "/sfm_data.json",
-                    "-o" , self.matches_dir + "/pairs.bin" 
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_PairGenerator'),
+                    '-i', self.matches_dir + '/sfm_data.json',
+                    '-o' , self.matches_dir + '/pairs.bin' 
                     ])
         pPairs.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def compute_matches(self):
-        print ("4. Compute matches")
+        time_start = time.time()
+        print ('[{}] 4. Compute matches'.format(time_start))
         pMatches = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_ComputeMatches"),
-                   "-i", self.matches_dir + "/sfm_data.json",
-                   "-p", self.matches_dir + "/pairs.bin",
-                   "-o", self.matches_dir + "/matches.putative.bin"
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_ComputeMatches'),
+                   '-i', self.matches_dir + '/sfm_data.json',
+                   '-p', self.matches_dir + '/pairs.bin',
+                   '-o', self.matches_dir + '/matches.putative.bin'
                    ])
         pMatches.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def filter_matches(self):
-        print ("5. Filter matches" )
+        time_start = time.time()
+        print ('[{}] 5. Filter matches'.format(time_start))
         pFiltering = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_GeometricFilter"),
-                    "-i", self.matches_dir + "/sfm_data.json",
-                    "-m", self.matches_dir + "/matches.putative.bin" ,
-                    "-g",
-                    "e",
-                    "-o", self.matches_dir + "/matches.e.bin"
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_GeometricFilter'),
+                    '-i', self.matches_dir + '/sfm_data.json',
+                    '-m', self.matches_dir + '/matches.putative.bin' ,
+                    '-g',
+                    'e',
+                    '-o', self.matches_dir + '/matches.e.bin'
                     ])
         pFiltering.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def recon(self):
-        print ("6. Do Global reconstruction")
+        time_start = time.time()
+        print ('[{}] 6. Do Global reconstruction'.format(time_start))
         pRecons = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_SfM"),
-                    "--sfm_engine", "GLOBAL",
-                    "--input_file", self.matches_dir + "/sfm_data.json",
-                    "--match_file", self.matches_dir + "/matches.e.bin",
-                    "--output_dir", self.reconstruction_dir
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_SfM'),
+                    '--sfm_engine', 'GLOBAL',
+                    '--input_file', self.matches_dir + '/sfm_data.json',
+                    '--match_file', self.matches_dir + '/matches.e.bin',
+                    '--output_dir', self.reconstruction_dir
                     ])
         pRecons.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def colorize(self):
-        print ("7. Colorize Structure")
+        time_start = time.time()
+        print ('[{}] 7. Colorize Structure'.format(time_start))
         pRecons = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_ComputeSfM_DataColor"),
-                    "-i", self.reconstruction_dir + "/sfm_data.bin",
-                    "-o", os.path.join(reconstruction_dir,"colorized.ply")
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_ComputeSfM_DataColor'),
+                    '-i', self.reconstruction_dir + '/sfm_data.bin',
+                    '-o', os.path.join(reconstruction_dir,'colorized.ply')
                     ])
         pRecons.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
     def export_to_openmvs(self):
-        print ("8. Export to openMVS")
+        time_start = time.time()
+        print ('[{}] 8. Export to openMVS'.format(time_start))
         pRecons = subprocess.Popen(
-                [os.path.join(OPENMVG_SFM_BIN, "openMVG_main_openMVG2openMVS"),
-                    "-i", self.reconstruction_dir + "/sfm_data.bin",
-                    "-o", os.path.join(self.mvs_dir,"scene.mvs"),
-                    "-d", self.mvs_dir
+                [os.path.join(OPENMVG_SFM_BIN, 'openMVG_main_openMVG2openMVS'),
+                    '-i', self.reconstruction_dir + '/sfm_data.bin',
+                    '-o', os.path.join(self.mvs_dir,'scene.mvs'),
+                    '-d', self.mvs_dir
                     ])
         pRecons.wait()
+        time_used = time.time() - time_start
+        print('Finished in {.0f}s'.format(time_used))
 
 
 def main():
     if len(sys.argv) < 3:
-        print ("Usage %s image_dir output_dir" % sys.argv[0])
+        print ('Usage %s image_dir output_dir' % sys.argv[0])
         sys.exit(1)
 
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
-    print ("Using input dir  : ", input_dir)
-    print ("      output_dir : ", output_dir)
+    print ('Using input dir  : ', input_dir)
+    print ('      output_dir : ', output_dir)
     recon = GlobalReconstructor(input_dir, output_dir)
-    # recon.list_images()
-    # recon.compute_features()
-    # recon.compute_pairs()
-    # recon.compute_matches()
-    # recon.filter_matches()
-    # recon.recon()
-    # recon.colorize()
+    recon.list_images()
+    recon.compute_features()
+    recon.compute_pairs()
+    recon.compute_matches()
+    recon.filter_matches()
+    recon.recon()
+    recon.colorize()
     recon.export_to_openmvs()
 
 
